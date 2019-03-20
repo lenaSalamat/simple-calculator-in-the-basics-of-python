f13d334a52c9131465a6bb5f6fbeaa80413056b2
lenaSalamat <lenasalamat96@gmail.com>
2019-03-20 18:22:18 +0200

    simple-calculator

--- calculator.py
+++ calculator.py
@@ -12,5 +12,25 @@ def mul(num1 ,num2):
 
 #return the result of dividing num1 - num2
 def div(num1 ,num2):
-	return num1 / num2	
+	return num1 / num2
+
+def main():
+	operation = input("what do you want to do (+,-,*,/) :")
+	if(operation != '+' and operation != '-' and operation != '*' and operation != '/'):
+		#invalid operation
+		print("you must enter a valid operation")
+	else:
+	    var1 = int(input("Enter num1 : "))	
+	    var2 = int(input("Enter num2 : "))
+	    if(operation == '+'):
+	    	print(add(var1 ,var2))
+	    elif(operation == '-'):
+	        print(sub(var1 ,var2))
+	    elif(operation == '*'):    	
+	        print(mul(var1 ,var2))
+	    else:
+	        print(div(var1 ,var2))
+
+main()    
+
 
