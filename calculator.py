5db993197396407547d02e8334e16393eb7ccdfd
lenaSalamat <lenasalamat96@gmail.com>
2019-03-20 18:18:08 +0200

    simple-calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#return the sum of num1 and num2
+def add(num1 ,num2):
+	return num1 + num2
+
+#return the result of subtracting num1 - num2
+def sub(num1 ,num2):
+	return num1 - num2
+
+#return the result of multiplying num1 - num2
+def mul(num1 ,num2):
+	return num1 * num2
+
+#return the result of dividing num1 - num2
+def div(num1 ,num2):
+	return num1 / num2	
+
